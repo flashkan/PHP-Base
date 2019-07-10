7af7245eefa547ce487d0cde539397c582fc10b6
Flashkan <nikol-babkin@yandex.ru>
2019-07-10 22:22:11 +0300

    6

---
+++ lesson_6/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'engine/autoload.php';
+autoload('config');
+include ENGINE_DIR.'lesson6.php';
+
+include PUBLIC_DIR.'header.php';
+include PUBLIC_DIR.'lesson6.php';
+include PUBLIC_DIR.'footer.php';
